9dd2147c563a6d00cf4dd7b6621690b341faaff2
Kuba <kubenstein@gmail.com>
2013-05-26 19:14:27 +0200

    remove in gem documentation

--- lib/just_include/just_include.rb
+++ lib/just_include/just_include.rb
@@ -1,39 +1,6 @@
 # JustInclude
+# see gem homepage for more info
 # https://github.com/kubenstein/just_include
-#
-#
-# JustInclude simply wraps included { class_eval { ... } } combo
-# class_eval is better then normal method ClassMethods because you can include all kind of things
-# you normally type inside a class. Such as:
-# - methods
-# - class methods
-# - callbacks
-# - relation definitions
-# - act_as_*
-# - constans
-# - extends / includes
-# etc...
-#
-# example:
-#   module Abc
-#     include JustInclude
-#     just_include do
-#
-#       extend FriendlyId
-#
-#       acts_as_taggable_on :skills
-#
-#       searchable do
-#       end
-#
-#       def method
-#       end
-#
-#       def self.class_method
-#       end
-#
-#     end
-#   end
 
 module JustInclude
 
